e27ab01d26daba5e8341bc889ab64a658d3d0786
JackFTCC <solliscj4556@student.faytechcc.edu>
2025-01-27 18:34:45 +0000

    More cout messages and new name

---
+++ m1t2_sollisch.cpp
@@ -0,0 +1,18 @@
+//CSC 134
+//Jack Sollisch
+// 1/27/2025
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Hello, World!" << endl;
+    cout <<"Bro is programming, bro is programmingpilled, programmercelled, and going programmermode" << endl;
+    cout <<"I like Jujustu Kaisen" << endl;
+    cout <<"I have a full collection of jjk cards, the first volume, and watched the show 5 times over." << endl;
+    cout <<"I also like Union Arena" << endl;
+    cout <<"I've placed in 2/3 events, one in first place." << endl;
+    return 0;
+}
