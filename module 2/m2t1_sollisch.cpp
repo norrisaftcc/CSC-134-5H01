b4846edabe50f8631e03539a1bdff7ce42de012d
JackFTCC <solliscj4556@student.faytechcc.edu>
2025-01-29 18:45:14 +0000

    organizing files and new classwork

---
+++ module 2/m2t1_sollisch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    string customer_name;
+    int apples_to_buy;
+    const double APPLEPRICE = .25;
+    string farm_name = "Jack";
+
+    cout << "Hi, what's your name? ";
+    cin >> customer_name;
+    cout << "Nice to meet you, " << customer_name << " welcome to the " << farm_name << " farm!" << endl;
+
+    return 0;
+}
