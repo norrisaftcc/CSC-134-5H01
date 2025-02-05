86ab39887a473751ea84358e9e3dcf26702ebb76
JackFTCC <solliscj4556@student.faytechcc.edu>
2025-02-05 14:00:34 -0500

    CoolS Turtle

---
+++ module 2/toitle.py
@@ -0,0 +1,113 @@
+from turtle import *
+
+t = Turtle()
+t.pencolor("blue")
+t.pensize(5)
+t.speed(0)
+t.shape("turtle")
+
+t.right(90)
+t.forward (100)
+t.left(90)
+t.penup()
+t.forward(70)
+t.pendown()
+t.left(90)
+t.forward(100)
+t.left(90)
+t.penup()
+t.forward(140)
+t.left(90)
+t.pendown()
+t.forward(100)
+
+#First Three Lines
+
+t.left(180)
+t.penup()
+t.forward(200)
+t.pendown()
+t.forward(100)
+t.right(90)
+t.penup()
+t.forward (70)
+t.pendown()
+t.right(90)
+t.forward(100)
+t.left(90)
+t.penup()
+t.forward(70)
+t.pendown()
+t.left(90)
+t.forward(100)
+
+#Second Three Lines
+
+t.left(90)
+t.penup()
+t.forward(140)
+t.left(90)
+t.forward(100)
+t.pendown()
+t.left(35)
+t.forward(120)
+
+#First Slash
+
+t.left(55)
+t.penup()
+t.forward(70)
+t.left(125)
+t.pendown()
+t.forward(120)
+
+#Second Slash
+
+t.penup()
+t.forward(120)
+t.right(80)
+t.pendown()
+t.forward(100)
+
+# First Closing
+
+t.right(90)
+t.forward(100)
+
+# Second Closing
+
+t.right(45)
+t.penup()
+t.forward(300)
+t.right(50)
+t.pendown()
+t.forward(100)
+t.right(85)
+t.forward(100)
+
+# Third and Fourth Closing
+
+t.right(47)
+t.penup()
+t.forward(100)
+t.pendown()
+t.right(40)
+t.forward(55)
+
+# Closing Slash
+
+t.penup()
+t.forward(158)
+t.right(180)
+t.left(45)
+t.forward(55)
+t.right(45)
+t.pendown()
+t.forward(65)
+t.hideturtle()
+
+
+
+
+
+
