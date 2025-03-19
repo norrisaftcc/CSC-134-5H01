51c6abcb54a697f3e05d394ddd5e8e32b53668ef
JackFTCC <solliscj4556@student.faytechcc.edu>
2025-03-19 18:41:56 +0000

    Temp

---
+++ Twine/hello.cpp
@@ -0,0 +1,13 @@
+//Jack Sollisch
+// 1/15/2025
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Hello, World!" << endl;
+    cout <<"Bro is programming, bro is programmingpilled, programmercelled, and going programmermode" << endl;
+    return 0;
+}
