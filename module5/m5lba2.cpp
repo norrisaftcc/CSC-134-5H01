52f8dca5b6e24188c73b3da5ae4dd609662b3c0a
JackFTCC <solliscj4556@student.faytechcc.edu>
2025-04-03 18:36:15 +0000

    m5lab2

--- module5/m5lba2.cpp
+++ module5/m5lba2.cpp
@@ -3,54 +3,36 @@
 #include <cmath>
 #include <ctime>
 #include <cstdlib>
-#include <fstream>
 #include <sstream>
 #include <map>
 #include <algorithm>
 #include <cctype>
 using namespace std;
 
-//double getLength();
-//double getWidth();
-void experiencePoints();
-int level;
-double levels;
+void experiencePoints(int& experience, int& level, int& levelUp);
 
 int main() {
-//    double length = getLength();
-//    double width = getWidth();
-//    if (length == width) {
-//        cout << "The area of the square is " << length * width << endl;
-//        cout << "The perimeter of the square is " << length + length + width + width << endl;
-//    } else {
-//        cout << "The area of the rectangle is " << length * width << endl;
-//        cout << "The perimeter of the rectangle is " << length + length + width + width << endl;
-//    }
+    int choice = 1;
+    int experience = 0;
+    int level = 1;
+    int levelUp = 1000;
 
-    experiencePoints();
+    do {
+        experiencePoints(experience, level, levelUp);
+        cout << "Do you want to run this program again (1 for yes, 0 for no): ";
+        cin >> choice;
+    } while (choice == 1);
 
     return 0;
 }
 
-//double getLength() {
-//    double length;
-//    cout << "Enter the length of the shape: ";
-//    return length;
-//}
-//
-//double getWidth() {
-//    double width;
-//    cout << "Enter the width of the shape: ";
-//    return width;
-//}
+void experiencePoints(int& experience, int& level, int& levelUp) {
+    int experienceAdd;
 
-void experiencePoints() {
-    int level = 1;
-    int experience;
-    int levelUp = 1000;
+    cout << "Enter experience to add: ";
+    cin >> experienceAdd;
 
-    cout << "Enter experience: ";
-    cin >> experience;
+    experience += experienceAdd;
 
     while (experience >= levelUp) {
         experience -= levelUp;
